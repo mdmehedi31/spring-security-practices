e985ae862e2c32d05ff5ac237d8a6a58eaadc74b
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-05-23 12:05:03 +0600

    custom filter

---
+++ src/main/java/com/sps/filter/LoggingFilter.java
@@ -0,0 +1,23 @@
+package com.sps.filter;
+
+
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+
+@Configuration
+public class LoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        System.out.println("From Custom Filter :: The request url is : "+httpRequest.getRequestURI());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
