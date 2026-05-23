964c7f623398817ce9bca3696e17b3faffafb90c
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-05-23 09:44:19 +0600

    init commit

---
+++ src/main/java/com/sps/SpringSecurityPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.sps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPracticesApplication.class, args);
+    }
+
+}
