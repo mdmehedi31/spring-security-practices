69b9e60d037b30cb50d3af30b719cfa67fdef7eb
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-05-23 21:43:07 +0600

    custom filter

--- src/main/java/com/sps/interceptor/LoggingInterceptor.java
+++ src/main/java/com/sps/interceptor/LoggingInterceptor.java
@@ -3,26 +3,37 @@ package com.sps.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class LoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingInterceptor.class);
+    private Integer requestCount = 0;
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
 
-        System.out.println("This is from post handler : " + request.getRequestURI());
+        log.info("This is from post handler : " + request.getRequestURI());
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
-        System.out.println("This is from after handler : " + request.getRequestURI());
+        log.info("This is from after handler : " + request.getRequestURI());
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("This is from pre handler : " + request.getRequestURI());
+
+        log.info("This is pre handler : " +  requestCount);
+        if(requestCount>3){
+            return false;
+        }
+        this.requestCount++;
+        log.info("This is from pre handler : " + request.getRequestURI());
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 }
