ae2c5a04acd8968e8130fa2bfd20ba2b684a40c2
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-05-26 09:26:39 +0600

    security filter chain config enabled

---
+++ src/main/java/com/sps/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.sps.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    BasicAuthenticationFilter basicAuthenticationFilter;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                auth->auth.anyRequest().
+                        authenticated()).
+                httpBasic(withDefaults());
+        return http.build();
+    }
+}
