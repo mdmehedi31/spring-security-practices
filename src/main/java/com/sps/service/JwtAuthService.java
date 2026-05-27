dcff3e1d3571f3e36e65488775879ae01cc56d15
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-05-27 11:55:07 +0600

    jwt token generate impl

--- src/main/java/com/sps/service/JwtAuthService.java
+++ src/main/java/com/sps/service/JwtAuthService.java
@@ -2,6 +2,9 @@ package com.sps.service;
 
 
 import com.sps.entity.AuthRequest;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +12,21 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class JwtAuthService {
 
+    private static final long  EXPIRIATION_AT = 1000*60*60;
     @Autowired
     private AuthenticationManager authenticationManager;
     private static final Logger log = LoggerFactory.getLogger(JwtAuthService.class);
+    private final String key = "this is the spring practices code base at token generated key";
+    private final SecretKey secretKey= Keys.hmacShaKeyFor(key.getBytes()); //Keys.hmacShaKeyFor(key.getBytes());
 
     public Map<String, String> generateJwtToken(AuthRequest authRequest) {
         try{
@@ -27,11 +36,26 @@ public class JwtAuthService {
                     new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
             );
 
-            token.put("token", "token is");
+            String generatedToken=getToken(authRequest.getUsername());
+            token.put("token", generatedToken);
 
             return token;
         }catch (Exception e) {
             log.error("An exception thrown when generating the jwt token", e);
+            throw e;
+        }
+    }
+
+
+    private String getToken(String userName) {
+        try{
+            return  Jwts.builder().setSubject(userName).
+                    setIssuedAt(new Date()).
+                    setExpiration(new Date(System.currentTimeMillis() + EXPIRIATION_AT)).
+                    signWith(secretKey, SignatureAlgorithm.HS256)
+                    .compact();
+
+        }catch (Exception e) {
             throw e;
         }
     }
